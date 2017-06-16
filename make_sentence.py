9908136f1615279abc167d94ae978786ea634daf
Madelyn Broome <mbroome@princeton.edu>
2017-06-16 10:35:44 -0400

    make sentence python program still broken

---
+++ make_sentence.py
@@ -0,0 +1,24 @@
+print('Enter a word (. ! or ? to end):')
+a = str(input())
+# def makesentence(a):
+# 	if a.str() != '.':
+# 		print('Enter a word (. ! or ? to end):')
+# 	# if a != '!':
+# 	# break a = '.'
+# 	else:
+# 		break
+
+# print('%r'+'%r' %(a,b))	
+
+
+for word in a:    
+   if word == '!':
+      break
+   elif word == '.':
+   		break
+   elif word == '?':
+   		break
+   else:
+   		print('Enter a word (. ! or ? to end):', word)	
+
+# print('%r'+'%r',format(a))
